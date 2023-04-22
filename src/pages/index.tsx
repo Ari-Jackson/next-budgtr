cbd802657f99d8e9a2255f40d33afa84ba16c3b6
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-22 17:37:59 -0400

    Solves transaction type error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   console.log(
-    api.transaction.getSingle.useQuery("clgr0o5ud0000yaaica1fgw1d").data
+    api.loggedTransaction.getSingle.useQuery("clgr0o5ud0000yaaica1fgw1d").data
   );
 
   return (
--- src/server/api/routers/transaction.ts
+++ src/server/api/routers/transaction.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const transactionRouter = createTRPCRouter({
+export const loggedTransactionRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.transaction.findMany();
   }),
   getSingle: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.transaction.findUnique({
       where: {
-        id: input,
+        transactionId: input,
       },
     });
   }),
