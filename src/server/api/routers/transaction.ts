a3a97cc9ab50a213e811eb4dcd739801a792e127
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-23 16:01:32 -0400

    Adds baisc routes for transaction api

--- src/server/api/routers/transaction.ts
+++ src/server/api/routers/transaction.ts
@@ -1,13 +1,50 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
+
+const newLogShape = z.object({
+  title: z.string(),
+  from: z.string(),
+  category: z.string(),
+  amount: z.number().nonnegative(),
+  deposit: z.boolean(),
+  date: z.bigint(),
+});
 
 export const loggedTransactionRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.transaction.findMany();
+  getAll: privateProcedure.query(({ ctx }) => {
+    return ctx.prisma.loggedTransaction.findMany({
+      where: {
+        authorid: ctx.userId,
+      },
+    });
   }),
-  getSingle: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return ctx.prisma.transaction.findUnique({
+  getLog: privateProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.prisma.loggedTransaction.findUnique({
+      where: {
+        transactionId: input,
+      },
+    });
+  }),
+  createLog: privateProcedure
+    .input(newLogShape)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.loggedTransaction.create({
+        data: { ...input, authorid: ctx.userId },
+      });
+    }),
+  updateLog: privateProcedure
+    .input(z.object({ content: newLogShape, transactionId: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.loggedTransaction.update({
+        where: {
+          transactionId: input.transactionId,
+        },
+        data: input,
+      });
+    }),
+  deleteLog: privateProcedure.input(z.string()).mutation(({ ctx, input }) => {
+    return ctx.prisma.loggedTransaction.delete({
       where: {
         transactionId: input,
       },
