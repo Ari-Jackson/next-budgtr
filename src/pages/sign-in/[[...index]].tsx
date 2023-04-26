dfa13ff5ad290b8d1aba825a24c2595e5331be76
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-26 16:09:29 -0400

    Adds clerk sign-in/sign-up

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,6 +1,8 @@
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  <>
+    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  </>
 );
 export default SignInPage;
